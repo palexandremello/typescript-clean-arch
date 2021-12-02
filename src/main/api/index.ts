015347f83ca0e00f47d666e937311ae4994a914d
Paulo Alexandre Mello <palexandremello@gmail.com>
2021-12-01 22:45:52 -0300

    chore: typescript configs

---
+++ src/main/api/index.ts
@@ -0,0 +1,7 @@
+import "../config/module-alias";
+import { PersonController } from "@/application/controllers/person";
+
+const p = new PersonController();
+
+console.log(p.speak("paulo"));
+console.log(p.speak());
