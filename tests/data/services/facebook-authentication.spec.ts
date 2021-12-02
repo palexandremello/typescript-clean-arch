547409c9a4234c502f90ab59010de8e49afcacd6
Paulo Alexandre Mello <palexandremello@gmail.com>
2021-12-02 00:08:44 -0300

    move LoadFacebookUserApi to its own file

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,3 +1,4 @@
+import { LoadFacebookUserApi } from '@/data/contracts/apis/facebook'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 
@@ -8,18 +9,6 @@ class FacebookAuthenticationService {
     await this.LoadFacebookUserApi.loadUser(params)
     return new AuthenticationError()
   }
-}
-interface LoadFacebookUserApi {
-  loadUser: (params: LoadFacebookUserApi.Params) => Promise<LoadFacebookUserApi.Result>
-
-}
-
-namespace LoadFacebookUserApi {
-  export type Params = {
-    token: string
-  }
-
-  export type Result = undefined
 }
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
