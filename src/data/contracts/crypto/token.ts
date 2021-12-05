fd588e6a14c03839843f50549c219079d6312a1c
Paulo Alexandre Mello <palexandremello@gmail.com>
2021-12-04 22:29:28 -0300

    ensure Service returns an AccessToken on success

--- src/data/contracts/crypto/token.ts
+++ src/data/contracts/crypto/token.ts
@@ -1,10 +1,13 @@
+export interface TokenGenerator {
+  generateToken: (params: TokenGenerator.Params) => Promise<TokenGenerator.Result>
+}
+
 export namespace TokenGenerator {
 
   export type Params = {
     key: string
     expirationInMs: number
   }
-}
-export interface TokenGenerator {
-  generateToken: (params: TokenGenerator.Params) => Promise<void>
+
+  export type Result = string
 }
--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -5,7 +5,7 @@ import { AccessToken, FacebookAccount } from '@/domain/models'
 import { TokenGenerator } from '../contracts/crypto'
 import { LoadUserAccountRepository, SaveFacebookAccountRepository } from '../contracts/repos'
 
-export class FacebookAuthenticationService {
+export class FacebookAuthenticationService implements FacebookAuthentication {
   constructor (
     private readonly facebookApi: LoadFacebookUserApi,
     private readonly userAccountRepo: LoadUserAccountRepository & SaveFacebookAccountRepository,
@@ -13,13 +13,14 @@ export class FacebookAuthenticationService {
 
   ) {}
 
-  async perfom (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
+  async perfom (params: FacebookAuthentication.Params): Promise<FacebookAuthentication.Result> {
     const fbData = await this.facebookApi.loadUser(params)
     if (fbData !== undefined) {
       const accountData = await this.userAccountRepo.load({ email: fbData.email })
       const fbAccount = new FacebookAccount(fbData, accountData)
       const { id } = await this.userAccountRepo.saveWithFacebook(fbAccount)
-      await this.crypto.generateToken({ key: id, expirationInMs: AccessToken.expirationInMs })
+      const token = await this.crypto.generateToken({ key: id, expirationInMs: AccessToken.expirationInMs })
+      return new AccessToken(token)
     }
 
     return new AuthenticationError()
