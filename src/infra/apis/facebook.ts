5591110f82d627838d4d293120c2570d94a3b809
Paulo Alexandre Mello <palexandremello@gmail.com>
2021-12-05 01:49:48 -0300

    improve code quality

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -1,7 +1,22 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { HttpGetClient } from '../http'
 
-export class FacebookApi {
+type DebugToken = {
+  data: {
+    user_id: string
+  }
+}
+
+type AppToken = {
+  access_token: string
+}
+
+type UserInfo = {
+  id: string
+  name: string
+  email: string
+}
+export class FacebookApi implements LoadFacebookUserApi {
   private readonly baseUrl = 'https://graph.facebook.com/oauth'
   constructor (
     private readonly httpClient: HttpGetClient,
@@ -9,8 +24,17 @@ export class FacebookApi {
     private readonly clientSecret: string
   ) {}
 
-  async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
-    const appToken = await this.httpClient.get({
+  async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
+    const userInfo = await this.getUserInfo(params.token)
+    return {
+      facebookId: userInfo.id,
+      name: userInfo.name,
+      email: userInfo.email
+    }
+  }
+
+  private async getAppToken (): Promise<AppToken> {
+    return this.httpClient.get({
       url: `${this.baseUrl}/access_token`,
       params: {
         client_id: this.clientId,
@@ -19,21 +43,29 @@ export class FacebookApi {
 
       }
     })
-    const debugToken = await this.httpClient.get({
+  }
+
+  private async getDebugToken (clientToken: string): Promise<DebugToken> {
+    const appToken = await this.getAppToken()
+
+    return this.httpClient.get({
       url: `${this.baseUrl}/debug_token`,
       params: {
         access_token: appToken.access_token,
-        input_token: params.token
+        input_token: clientToken
 
       }
     })
+  }
 
-    await this.httpClient.get({
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  private async getUserInfo (clientToken: string): Promise<UserInfo> {
+    const debugToken = await this.getDebugToken(clientToken)
+
+    return this.httpClient.get({
       url: `${this.baseUrl}/${debugToken.data.user_id}`,
       params: {
         fields: ['id', 'name', 'email'].join(','),
-        access_token: params.token
+        access_token: clientToken
 
       }
     })
--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -18,6 +18,8 @@ describe('FacebookApi', () => {
     httpClient.get
       .mockResolvedValueOnce({ access_token: 'any_app_token' })
       .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+      .mockResolvedValueOnce({ id: 'any_fb_id', name: 'any_fb_name', email: 'any_fb_email' })
+
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
   it('Should get app token', async () => {
@@ -54,6 +56,16 @@ describe('FacebookApi', () => {
         fields: 'id,name,email',
         access_token: 'any_client_token'
       }
+    })
+  })
+
+  it('Should return facebook user', async () => {
+    const facebookUser = await sut.loadUser({ token: 'any_client_token' })
+
+    expect(facebookUser).toEqual({
+      facebookId: 'any_fb_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email'
     })
   })
 })
