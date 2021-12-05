88f3ae20065b2acd50d52de5f2376f10aa392ef6
Paulo Alexandre Mello <palexandremello@gmail.com>
2021-12-05 00:51:00 -0300

    ensure FacebookApi get app token with correct url

--- src/main/api/index.ts
+++
@@ -1,7 +0,0 @@
-import '../config/module-alias'
-import { PersonController } from '@/application/controllers/person'
-
-const p = new PersonController()
-
-console.log(p.speak('paulo'))
-console.log(p.speak())
