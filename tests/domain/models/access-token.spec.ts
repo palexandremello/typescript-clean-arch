acdd15521c100468d95cc6be0885b3a304fccb55
Paulo Alexandre Mello <palexandremello@gmail.com>
2021-12-04 21:24:43 -0300

    ensure AccessToken has a value

---
+++ tests/domain/models/access-token.spec.ts
@@ -0,0 +1,8 @@
+import { AccessToken } from '@/domain/models'
+
+describe('AccessToken', () => {
+  it('Should create with a value', () => {
+    const sut = new AccessToken('any_value')
+    expect(sut).toEqual({ value: 'any_value' })
+  })
+})
