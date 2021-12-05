d2ce3330c940697b08edcd4cba2148e45afea03e
Paulo Alexandre Mello <palexandremello@gmail.com>
2021-12-04 21:28:27 -0300

    ensure AccessToken expires in 30 minutes

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,3 +1,10 @@
 export class AccessToken {
   constructor (private readonly value: string) {}
+
+  static get expirationInMs (): number {
+    const minutes = 30
+    const seconds = 60
+    const miliseconds = 1000
+    return minutes * seconds * miliseconds
+  }
 }
