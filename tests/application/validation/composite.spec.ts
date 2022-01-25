1588a43c5bbd8be269af09718e0a2591e4248fd2
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-01-25 00:48:56 -0300

    ensure ValidationComposite returns error

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -35,18 +35,16 @@ describe('ValidationComposite', () => {
     expect(error).toBeUndefined()
   })
   it('should return the first error', () => {
-    validator1.validate.mockReturnValue(new Error('error_1'))
-    validator2.validate.mockReturnValue(new Error('error_2'))
+    validator1.validate.mockReturnValueOnce(new Error('error_1'))
+    validator2.validate.mockReturnValueOnce(new Error('error_2'))
 
     const error = sut.validate()
     expect(error).toEqual(new Error('error_1'))
   })
-  it('should return the first error', () => {
-    validator1.validate.mockReturnValue(new Error('error_1'))
-    validator2.validate.mockReturnValue(new Error('error_2'))
+  it('should return the  error', () => {
+    validator2.validate.mockReturnValueOnce(new Error('error_2'))
 
     const error = sut.validate()
-    expect(error).toEqual(new Error('error_1'))
+    expect(error).toEqual(new Error('error_2'))
   })
-
 })
