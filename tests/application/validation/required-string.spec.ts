e75ac9dfff511c67cde4caafff6599ba840fcfaf
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-01-25 00:08:23 -0300

    ensure RequiredStringValidator returns a erro

--- tests/application/validation/required-string.spec.ts
+++ tests/application/validation/required-string.spec.ts
@@ -13,7 +13,21 @@ class RequiredStringValidator {
 
 describe('RequiredStringValidator', () => {
   it('Should return Error if value is empty', () => {
+    const sut = new RequiredStringValidator('', 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+  it('Should return Error if value is null', () => {
     const sut = new RequiredStringValidator(null as any, 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+  it('Should return Error if value is undefined', () => {
+    const sut = new RequiredStringValidator(undefined as any, 'any_field')
 
     const error = sut.validate()
 
