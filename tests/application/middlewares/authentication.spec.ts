c7e5709e691ff80ee4315e72b1f8c2dee315e744
Paulo Alexandre Mello <palexandremello@gmail.com>
2022-02-15 00:38:02 -0300

    ensure AuthenticationMiddleware returns 403 if authorization is undefined and null

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -22,4 +22,25 @@ describe('AuthenticationMiddleware', () => {
       data: new ForbiddenError()
     })
   })
+  it('should return 403 if authorization is null', async () => {
+    const sut = new AuthenticationMiddleware()
+
+    const httpResponse = await sut.handle({ authorization: null as any })
+
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
+  })
+
+  it('should return 403 if authorization is undefined', async () => {
+    const sut = new AuthenticationMiddleware()
+
+    const httpResponse = await sut.handle({ authorization: undefined as any })
+
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
+  })
 })
